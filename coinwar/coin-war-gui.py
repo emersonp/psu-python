b0377d185a8a35281385945c06533e1c6f30e07e
Parker Harris Emerson <parker.emerson@gmail.com>
2014-07-29 10:11:36 -0700

    Added Comments to CW
    Added explanatory comments documenting program to coin-war-gui.py

--- coinwar/coin-war-gui.py
+++ coinwar/coin-war-gui.py
@@ -40,17 +40,19 @@ class CoinWar(Frame):
     self.player2_display = Label(self)
     self.player2_display.grid(row = 1, column = 3, sticky = E + W)
 
-    # Play Game
+    # Game Display Label
     self.game_display = Label(self, justify = LEFT)
     self.game_display.grid(row = 2, column = 0, columnspan = 8, stick = E + W)
 
   def do_play(self):
+    # Reset Variables
     self.player1_coins = ""
     self.player2_coins = ""
     self.player1_prisoners = ""
     self.player2_prisoners = ""
     self.game_play_text = ""
 
+    # Test Text Entry Boxes for Validity, and If Valid Accept Text
     self.temp_coins = self.player1_enter.get().upper()
     self.temp_coins_count = self.temp_coins.count("H") + self.temp_coins.count("T")
     if self.temp_coins_count >= 5 and len(self.temp_coins) == self.temp_coins_count:
@@ -61,6 +63,7 @@ class CoinWar(Frame):
     if self.temp_coins_count >= 5 and len(self.temp_coins) == self.temp_coins_count:
       self.player2_coins = self.temp_coin
 
+    # If No Text Entered, Randomly Generate Heads/Tails
     if len(self.player1_coins) == 0:
       for i in range(5):
         if random.randrange(2) > 0:
@@ -74,9 +77,12 @@ class CoinWar(Frame):
         else:
           self.player2_coins += "T"
 
+    # Play Game
     while len(self.player1_coins) > 0 and len(self.player2_coins) > 0:
       self.game_play_text += "\nPlayer 1 Coins: " + self.player1_coins +  "\tP: " + self.player1_prisoners
       self.game_play_text += "\nPlayer 2 Coins: " + self.player2_coins +  "\tP: " + self.player2_prisoners
+
+      # If Player First Coins are the Same...
       if self.player1_coins[0] == self.player2_coins[0]:
         self.game_play_text += "\nRound is a tie!"
         self.player1_prisoners += self.player1_coins[0]
@@ -89,6 +95,8 @@ class CoinWar(Frame):
         if len(self.player2_coins) > 0:
           self.player2_prisoners += self.player2_coins[0]
           self.player2_coins = self.player2_coins[1:]
+
+      # Else, If Each Player Has Heads...
       elif self.player2_coins[0] == "H":
         self.game_play_text += "\nPlayer 2 wins the Round!"
         self.player2_coins = self.player2_coins[1:] + "TH" + self.player1_prisoners + self.player2_prisoners
@@ -102,8 +110,10 @@ class CoinWar(Frame):
         self.player1_prisoners = ""
         self.player2_prisoners = ""
 
-      self.game_display.configure(text = self.game_play_text)
+    # Display Game Text
+    self.game_display.configure(text = self.game_play_text)
 
+    # Evaluate For Win Conditions
     self.game_play_text += "\nPlayer 1 Coins: " + self.player1_coins + "\tP:" + self.player1_prisoners
     self.game_play_text += "\nPlayer 2 Coins: " + self.player2_coins + "\tP:" + self.player2_prisoners
     if self.player1_coins > self.player2_coins:
